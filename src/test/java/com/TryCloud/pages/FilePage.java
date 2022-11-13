e7bee5730d53306d53846180025ee38821f362d2
Cydeo-ElenaCh <chernikhlena@gmail.com>
2022-11-13 16:30:05 -0500

    US_07

--- src/test/java/com/TryCloud/pages/FilePage.java
+++ src/test/java/com/TryCloud/pages/FilePage.java
@@ -11,7 +11,8 @@ import java.util.List;
 public class FilePage {
 
         public FilePage(){
-            PageFactory.initElements(Driver.getDriver(), this);
+
+                PageFactory.initElements(Driver.getDriver(), this);
         }
 
         @FindBy(xpath = "//label[@for='select_all_files']")
@@ -64,6 +65,52 @@ public class FilePage {
         @FindBy(xpath = "//a[@class='button new']")
         public WebElement addIcon;
 
-        @FindBy(xpath = "//label[@for='file_upload_start']")
+
+        @FindBy(xpath = "//label[@data-action='upload']")
         public WebElement uploadFile;
+
+
+
+
+        // US_07 elements
+        /////////
+        @FindBy(xpath = "//a[@aria-label='Files']")
+        public WebElement fileBtn;
+
+        @FindBy(xpath = "//a[@class='button new']")
+        public WebElement addBtn;
+
+        @FindBy(xpath = "//a[@data-action='folder']")
+        public WebElement newFolderBtn;
+
+        @FindBy(xpath = "//input[@class='icon-confirm']")
+        public WebElement arrowBtn;
+
+        @FindBy(id = "view13-input-folder")
+        public WebElement inputFolder;
+
+        @FindBy(xpath = "//span[@class='innernametext']")
+        public List<WebElement> folderNameList;
+
+        @FindBy(xpath = "//span[@id ='selectedActionsList']")
+        public WebElement actionsBtn;
+
+        @FindBy(xpath = "//span[.='Delete']")
+        public WebElement deleteBtn;
+
+        @FindBy(xpath = "//*[@id='file_upload_start']")
+        public WebElement inputFile;
+
+        @FindBy(css = "#uploadprogressbar")
+        public WebElement loadBar;
+
+        @FindBy(xpath = "//span[@class='innernametext']")
+        public List <WebElement> listOfFoldersNamesText;
+
+        //Inside the New Folder
+        @FindBy(xpath="//tr[@data-type='file']")
+        public List <WebElement> listOfUploadedFiles;
+
+        // End of US_07 elements
+
 }
---
+++ src/test/java/com/TryCloud/step_definitions/US_07_AddTheFolder_StepDef.java
@@ -0,0 +1,117 @@
+package com.TryCloud.step_definitions;
+import com.TryCloud.pages.FilePage;
+import com.TryCloud.utilities.BrowserUtils;
+import com.TryCloud.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class US_07_AddTheFolder_StepDef {
+    FilePage us7_page = new FilePage();
+    //LocalTime myObj = LocalTime.now();
+    //  WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
+    String folderName= "smth";
+
+
+
+    @When("user clicks the add icon on the top")
+    public void user_clicks_the_add_icon_on_the_top() {
+        BrowserUtils.waitFor(5);
+        us7_page.addBtn.click();
+
+
+    }
+
+    @When("user click 'new folder'")
+    public void user_click_new_folder() {
+        us7_page.newFolderBtn.click();
+
+    }
+
+    @When("user write a folder name")
+    public void user_write_a_folder_name() {
+
+        us7_page.inputFolder.sendKeys(folderName);
+
+    }
+
+    @When("the user click submit icon")
+    public void the_user_click_submit_icon() {
+        //us7_page.inputFolder.sendKeys(folderName + Keys.ENTER);
+        us7_page.arrowBtn.click();
+
+    }
+
+    @Then("Verify the folder is displayed on the page")
+    public void verify_the_folder_is_displayed_on_the_page() {
+        BrowserUtils.waitFor(1);
+        boolean a = false;
+        for (int i = 0; i < us7_page.folderNameList.size(); i++) {
+            if (us7_page.folderNameList.get(i).getText().equals(folderName)) {
+                a = true;
+                newFolderIndex = i + 1;
+                break;
+            }
+        }
+        Assert.assertTrue(a);
+    }
+    @And("user choose a folder from the page")
+    public void user_choose_a_folder_from_the_page() {
+        WebElement chooseFolder = Driver.getDriver().findElement(By.xpath("//tr[@data-file='smth']"));
+        chooseFolder.click();
+                }
+
+    @When("the user clicks  the add button")
+    public void theUserClicksTheAddButton() {
+        us7_page.addBtn.click();
+    }
+
+
+    @When("the user uploads a file with the upload file option")
+    public void the_user_uploads_a_file_with_the_upload_file_option() {
+        String pathToFile= "C:/Users/chern/OneDrive/Desktop/test.txt";
+       // us7_page.uploadFile.click();
+        us7_page.inputFile.sendKeys(pathToFile);
+    }
+
+
+    String uploadedFile="test";
+    @Then("Verify the file is displayed on the page")
+    public void verify_the_file_is_displayed_on_the_page() {
+
+        Driver.getDriver().findElement(By.xpath("//tr[@data-file='test.txt']")).isDisplayed();
+    }
+
+
+
+    public int newFolderIndex;
+
+    @And("delete uploaded file")
+    public void deleteUploadedFile() {
+        us7_page.fileBtn.click();
+        WebElement newFolderCheckBox = Driver.getDriver().findElement(By.xpath("//tr[@data-file='smth']/td[@class='selection']"));
+        newFolderCheckBox.click();
+        us7_page.actionsBtn.click();
+        BrowserUtils.waitFor(1);
+        us7_page.deleteBtn.click();
+        BrowserUtils.waitFor(1);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
